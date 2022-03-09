c93de4a3406bd91bb3e50c8194fd904c1a490807
cantstopcoding <jonathan_koss@yahoo.com>
2022-03-09 12:40:11 -0500

    create todo mock data and expectation

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 describe 'Todos API', type: :request do
   it 'returns all todos' do
     get 'http://localhost:3000/api/v1/todos'
+    FactoryBot.create(:todo, title: 'Walk dogs')
+    FactoryBot.create(:todo, title: 'Do laundry')
+    FactoryBot.create(:todo, title: 'Mop floor')
 
     expect(response).to have_http_status(:success)
+    expect(JSON.parse(response.body).size).to eq(3)
   end
 end
